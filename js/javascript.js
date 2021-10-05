6492fdbfce6f0283424d814e29a663e9e26af93f
tomassantander <90361292+tomassantander@users.noreply.github.com>
2021-10-05 13:15:27 -0300

    Slider nuevo

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
  
-  $('.slider li').hide();
-  $('.slider li:first').show();
+  var imgItems = $('.slider li').length; //esta variable nos dice la cantidad de imagenes que tiene el slider
+  for(i = 1; i <= imgItems; i++){
+    $('.pagination').append('<li> <span class="fa fa-circle"></span></li>'); //agregamos un icono ciruclo para cada foto
+  }
+  
 
-  $('.pagination li:first').css({'color':'white'})
+  $('.slider li').hide(); //ocultamos las fotos del slider
+  $('.slider li:first').show(); //solo mostramos la primera de la lista
+
+  $('.pagination li:first').css({'color':'white'}) //señalamos el primer circulo haciendo referencia a la primera foto
+
+  $('.pagination li').click(pagination);
+
+  function pagination(){
+    var paginationPos = $(this).index()+1;
+
+    $('.slider li').hide();
+    $('.slider li:nth-child('+paginationPos+')').fadeIn(1500);
+    $('.pagination li').css({'color':'#606060'})
+    $(this).css({'color':'white'});
+  }
 
  });
 
